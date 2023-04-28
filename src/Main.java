5a5dcadfa69f4e8475e3cc7010eb8a5095a90e1b
Antonov333 <antonovsergei333@yahoo.com>
2023-04-28 12:31:43 +0300

    Created Person class, created objects by different constructors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!\n\nCourse 2, OOP topic, extra problem 2-2-3");
+
+        Person john = new Person("John Smith", 25);
+        Person someone = new Person();
+
+        System.out.println(john.toString());
+
+        System.out.println(someone.toString());
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    private String fullName;
+    private int age;
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public Person() {
+        fullName = " ";
+        age = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
